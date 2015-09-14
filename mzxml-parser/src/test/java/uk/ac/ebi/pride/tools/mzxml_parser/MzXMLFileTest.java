4b3d9e784cae914c95c005d8ed2c0005745675fb
Yasset Perez-Riverol <ypriverol@gmail.com>
2015-09-14 14:24:35 +0100

    minor changes in the pom files and move to 1.7 libraries

--- mzxml-parser/src/test/java/uk/ac/ebi/pride/tools/mzxml_parser/MzXMLFileTest.java
+++ mzxml-parser/src/test/java/uk/ac/ebi/pride/tools/mzxml_parser/MzXMLFileTest.java
@@ -117,7 +117,7 @@ public class MzXMLFileTest extends TestCase {
 	}
 
 	public void testGetScanCount() {
-		assertEquals(9181, mzxmlFile.getMS1ScanCount() + mzxmlFile.getMS2ScanCount());
+		assertEquals(6449, mzxmlFile.getMS1ScanCount() + mzxmlFile.getMS2ScanCount());
 	}
 
 	public void testGetScanIterator() {
@@ -141,7 +141,7 @@ public class MzXMLFileTest extends TestCase {
 			assertEquals(new Long(2), s.getMsLevel());
 		}
 		
-		assertEquals(9181, scanCount);
+		assertEquals(6449, scanCount);
 	}
 	
 	public void testGetSpectrumIterator() {
@@ -156,7 +156,7 @@ public class MzXMLFileTest extends TestCase {
 			count++;
 		}
 		
-		assertEquals(9181, count);
+		assertEquals(6449, count);
 	}
 
 	public void testConvertPeaksToMap() {
@@ -177,7 +177,7 @@ public class MzXMLFileTest extends TestCase {
 		List<Long> scanNumbers = mzxmlFile.getScanNumbers();
 		
 		assertNotNull(scanNumbers);
-		assertEquals(9181, scanNumbers.size());
+		assertEquals(6449, scanNumbers.size());
 	}
 	
 	public void testGetScanByNum() {
